57120b25e15355cf70b0938d1d45a5a146086641
Alina <98577574+kapyrina@users.noreply.github.com>
2023-06-19 19:36:56 +0300

    Main.java

---
+++ Задание/Main.java
@@ -0,0 +1,106 @@
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkList<Contact> contactList = new SingleLinkList<>();
+
+        contactList.addToEnd(new Contact(123, "Ромашкина Маргарита Романовна", "+79205098301"));
+        contactList.addToEnd(new Contact(151, "Лыжин Степан Степанович", "+79825032016"));
+        contactList.addToEnd(new Contact(332, "Калинин Артём Максимович", "+79505061123"));
+        contactList.addToEnd(new Contact(432, "Петров Николай Анатолиевич", "+79953060265"));
+        contactList.addToEnd(new Contact(556, "Зубин Даниил Андреевич", "+79560282004"));
+
+        for(Contact contact: contactList) {
+            System.out.println(contact);
+        }
+
+        contactList.reverse();
+
+        System.out.println("------------------------");
+
+        for(Contact contact: contactList) {
+            System.out.println(contact);
+        }
+    }
+
+    static class Contact {
+        int id;
+        String name;
+        String phone;
+
+        public Contact(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
+        }
+    }
+
+    public static class SingleLinkList<T> implements Iterable<T> {
+
+        ListItem<T> head;
+        ListItem<T> tail;
+
+        @Override
+        public Iterator<T> iterator() {
+            return new Iterator<T>() {
+                ListItem<T> current = head;
+
+                @Override
+                public boolean hasNext() {
+                    return current != null;
+                }
+
+                @Override
+                public T next() {
+                    T data = current.data;
+                    current = current.next;
+                    return data;
+                }
+            };
+        }
+
+        private static class ListItem<T> {
+            T data;
+            ListItem<T> next;
+        }
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void addToEnd(T item) {
+            ListItem<T> newItem = new ListItem<>();
+            newItem.data = item;
+            if (isEmpty()) {
+                head = newItem;
+                tail = newItem;
+            } else {
+                tail.next = newItem;
+                tail = newItem;
+            }
+        }
+
+        public void reverse() {
+            if (!isEmpty() && head.next != null) {
+                tail = head;
+                ListItem<T> current = head.next;
+                head.next = null;
+                while (current != null) {
+                    ListItem<T> next = current.next;
+                    current.next = head;
+                    head = current;
+                    current = next;
+                }
+            }
+        }
+    }
+}
